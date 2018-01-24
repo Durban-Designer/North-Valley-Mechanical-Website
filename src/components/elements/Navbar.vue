5d85af828590ee25bee0ed1c0f1ec2d53757b468
Avery Jordan Angel <kazy297@hotmail.com>
2018-01-24 12:01:08 -0700

    Revert "Revert "Merge branch 'master' into avery_Dev""
    This reverts commit 9a943d1882d3ca5419b6401b0c95d2386b2d59ac.

--- src/components/elements/Navbar.vue
+++ src/components/elements/Navbar.vue
@@ -67,162 +67,10 @@
 </script>
 
 <style scoped lang="less">
-@nvmgrey: #dae5ed;
-@nvmblue: #005389;
-@nvmred: #b20938;
+  @nvmgrey: #dae5ed;
+  @nvmblue: #005389;
+  @nvmred: #b20938;
 
-.navpane {
-  display: none;
-  overflow: hidden;
-}
-
-.main {
-  width: 100%;
-}
-
-.nv {
-  margin-top: 20px;
-  width: 80px;
-  height: 80px;
-  overflow: hidden;
-  margin-top: 10px;
-  margin-right: 10px;
-  grid-column: 4;
-  z-index: 8;
-  background-repeat: no-repeat;
-  background-image: url("../../assets/navbuttonAnimationWhite.svg");
-}
-
-.navbar {
-  width: 100%;
-  height: 100px;
-  background: linear-gradient(#005DA6, #014271);
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  z-index: 11;
-  text-align: center;
-  line-height: 70px;
-  font-weight: lighter;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr) 220px repeat(2,1fr);
-  grid-template-rows: 100px;
-}
-
-.navpaneAnimation {
-  animation: navpaneAnimation .3s steps(9);
-  animation-iteration-count: 1;
-  animation-fill-mode: forwards;
-  grid-column-start: 2;
-  grid-column-end: 5;
-  z-index: 10;
-  width: 100%;
-  height: 400px;
-  margin-top: 1px;
-  background: #b20938;
-  color: #fff;
-  outline: solid 1px ;
-  outline-color: #fff;
-  box-shadow: 0px 1px 1px 2px #fff;
-}
-
-.navpaneAnimationExit {
-  animation: navpaneAnimationReverse .3s steps(9);
-  animation-iteration-count: 1;
-  animation-fill-mode: forwards;
-  display: none;
-}
-
-.navpaneAnimationExit button {
-  display: none;
-}
-
-.Home {
-  box-shadow: none;
-  background-color: transparent;
-  border: none;
-  color: #fff;
-  font-size: 2.5em;
-  width: 100%;
-  font-weight: normal;
-}
-
-.Reviews {
-  background-color: transparent;
-  width: 100%;
-  background-color: transparent;
-  border: none;
-  color: #fff;
-  font-size: 2.5em;
-  font-weight: normal;
-}
-
-.Contact {
-  background-color: transparent;
-  width: 100%;
-  background-color: transparent;
-  border: none;
-  color: #fff;
-  font-size: 2.5em;
-  font-weight: normal;
-}
-
-.Services {
-  background-color: transparent;
-  width: 100%;
-  background-color: transparent;
-  border: none;
-  color: #fff;
-  font-size: 2.5em;
-  font-weight: normal;
-}
-
-.Login {
-  background-color: transparent;
-  width: 100%;
-  border: none;
-  color: #fff;
-  font-size: 2.5em;
-  font-weight: normal;
-}
-
-@keyframes navpaneAnimation {
-  0% { opacity: 0; }
-  100% { opacity: 1; }
-}
-
-@keyframes navpaneAnimationReverse {
-  0% { opacity: 1; }
-  100% { opacity: 0; display: none;}
-}
-
-@keyframes navButtonAnimation {
-  100% {background-position: -1440px;}
-}
-
-@keyframes navButtonAnimationReverse {
-  0% {background-position: -1440px;}
-  100% {background-position: 0px;}
-}
-
-.navButtonAnimation {
-  animation: navButtonAnimation .35s steps(18);
-  animation-iteration-count: 1;
-  animation-fill-mode: forwards;
-}
-
-.navButtonAnimationExit {
-  animation: navButtonAnimationReverse .35s steps(18);
-  animation-iteration-count: 1;
-  animation-fill-mode: forwards;
-}
-
-.hidden {
-  display: none;
-}
-
-@media (min-width: 700px) {
   .navpane {
     display: none;
     overflow: hidden;
@@ -333,7 +181,6 @@
   .Login {
     background-color: transparent;
     width: 100%;
-    background-color: transparent;
     border: none;
     color: #fff;
     font-size: 2.5em;
@@ -377,9 +224,36 @@
 
   @media (min-width: 700px) {
     .navpane {
+      display: none;
+      overflow: hidden;
+    }
+
+    .main {
       width: 100%;
-      height: 120px;
-      z-index: 3;
+    }
+
+    .nv {
+      margin-top: 20px;
+      width: 80px;
+      height: 80px;
+      overflow: hidden;
+      margin-top: 10px;
+      margin-right: 10px;
+      grid-column: 4;
+      z-index: 8;
+      background-repeat: no-repeat;
+      background-image: url("../../assets/navbuttonAnimationWhite.svg");
+    }
+
+    .navbar {
+      width: 100%;
+      height: 100px;
+      background: linear-gradient(#005DA6, #014271);
+      position: fixed;
+      top: 0;
+      left: 0;
+      right: 0;
+      z-index: 11;
       text-align: center;
       line-height: 70px;
       font-weight: lighter;
@@ -388,8 +262,134 @@
       grid-template-rows: 100px;
     }
 
-    .nv {
+    .navpaneAnimation {
+      animation: navpaneAnimation .3s steps(9);
+      animation-iteration-count: 1;
+      animation-fill-mode: forwards;
+      grid-column-start: 2;
+      grid-column-end: 5;
+      z-index: 10;
+      width: 100%;
+      height: 400px;
+      margin-top: 1px;
+      background: #b20938;
+      color: #fff;
+      outline: solid 1px ;
+      outline-color: #fff;
+      box-shadow: 0px 1px 1px 2px #fff;
+    }
+
+    .navpaneAnimationExit {
+      animation: navpaneAnimationReverse .3s steps(9);
+      animation-iteration-count: 1;
+      animation-fill-mode: forwards;
       display: none;
     }
-  }
+
+    .navpaneAnimationExit button {
+      display: none;
+    }
+
+    .Home {
+      box-shadow: none;
+      background-color: transparent;
+      border: none;
+      color: #fff;
+      font-size: 2.5em;
+      width: 100%;
+      font-weight: normal;
+    }
+
+    .Reviews {
+      background-color: transparent;
+      width: 100%;
+      background-color: transparent;
+      border: none;
+      color: #fff;
+      font-size: 2.5em;
+      font-weight: normal;
+    }
+
+    .Contact {
+      background-color: transparent;
+      width: 100%;
+      background-color: transparent;
+      border: none;
+      color: #fff;
+      font-size: 2.5em;
+      font-weight: normal;
+    }
+
+    .Services {
+      background-color: transparent;
+      width: 100%;
+      background-color: transparent;
+      border: none;
+      color: #fff;
+      font-size: 2.5em;
+      font-weight: normal;
+    }
+
+    .Login {
+      background-color: transparent;
+      width: 100%;
+      background-color: transparent;
+      border: none;
+      color: #fff;
+      font-size: 2.5em;
+      font-weight: normal;
+    }
+
+    @keyframes navpaneAnimation {
+      0% { opacity: 0; }
+      100% { opacity: 1; }
+    }
+
+    @keyframes navpaneAnimationReverse {
+      0% { opacity: 1; }
+      100% { opacity: 0; display: none;}
+    }
+
+    @keyframes navButtonAnimation {
+      100% {background-position: -1440px;}
+    }
+
+    @keyframes navButtonAnimationReverse {
+      0% {background-position: -1440px;}
+      100% {background-position: 0px;}
+    }
+
+    .navButtonAnimation {
+      animation: navButtonAnimation .35s steps(18);
+      animation-iteration-count: 1;
+      animation-fill-mode: forwards;
+    }
+
+    .navButtonAnimationExit {
+      animation: navButtonAnimationReverse .35s steps(18);
+      animation-iteration-count: 1;
+      animation-fill-mode: forwards;
+    }
+
+    .hidden {
+      display: none;
+    }
+
+    @media (min-width: 700px) {
+      .navpane {
+        width: 100%;
+        height: 120px;
+        z-index: 3;
+        text-align: center;
+        line-height: 70px;
+        font-weight: lighter;
+        display: grid;
+        grid-template-columns: repeat(2, 1fr) 220px repeat(2,1fr);
+        grid-template-rows: 100px;
+      }
+
+      .nv {
+        display: none;
+      }
+    }
 </style>
